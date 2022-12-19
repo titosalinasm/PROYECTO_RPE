89229ce2515ec6f30d0eb403573b18c2674f72f5
titosalinasm <titosalinasm@hotmail.com>
2022-12-18 21:44:24 -0500

    18/12/2022

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiGatewayMaestro: "http://20.236.129.139/maestro-",
+  apiGatewaySeguridad: "http://20.236.129.139/seguridad-",
+  recaptcha:{siteKey:''}
 };
